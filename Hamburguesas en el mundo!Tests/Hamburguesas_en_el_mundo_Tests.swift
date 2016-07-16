c60c251136fa96f147093c1ed3388c46e3582b7b
luiseduardo3004 <luiseduardo3004@gmail.com>
2016-07-16 03:50:43 -0500

    Initial Commit

---
+++ Hamburguesas en el mundo!Tests/Hamburguesas_en_el_mundo_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Hamburguesas_en_el_mundo_Tests.swift
+//  Hamburguesas en el mundo!Tests
+//
+//  Created by Luis Santamaría on 16/07/16.
+//  Copyright © 2016 Santamaria Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import Hamburguesas_en_el_mundo_
+
+class Hamburguesas_en_el_mundo_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
