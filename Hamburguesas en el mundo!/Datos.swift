6cea5a1aa951abb8474396b522c1175ef3e64d35
luiseduardo3004 <luiseduardo3004@gmail.com>
2016-07-16 04:10:02 -0500

    validación 1

---
+++ Hamburguesas en el mundo!/Datos.swift
@@ -0,0 +1,89 @@
+//
+//  Datos.swift
+//  Hamburguesas en el mundo!
+//
+//  Created by Luis Santamaría on 16/07/16.
+//  Copyright © 2016 Santamaria Technologies. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colores {
+    let colores = [
+        UIColor(red: 210/255.0, green: 90/255.0, blue: 45/255.0, alpha: 1),
+        UIColor(red: 40/255.0, green: 170/255.0, blue: 45/255.0, alpha: 1),
+        UIColor(red: 3/255.0, green: 180/255.0, blue: 90/255.0, alpha: 1),
+        UIColor(red: 210/255.0, green: 90/255.0, blue: 5/255.0, alpha: 1),
+        UIColor(red: 120/255.0, green: 120/255.0, blue: 50/255.0, alpha: 1),
+        UIColor(red: 130/255.0, green: 80/255.0, blue: 90/255.0, alpha: 1),
+        UIColor(red: 130/255.0, green: 130/255.0, blue: 130/255.0, alpha: 1),
+        UIColor(red: 3/255.0, green: 50/255.0, blue: 90/255.0, alpha: 1),
+        ]
+    
+    func coloresAleatorios() -> UIColor {
+        let posicion = Int ( arc4random()) % colores.count
+        return colores[posicion]
+    }
+}
+
+class ColeccionHamburguesas {
+    let hamburguesas = [
+        "Española",
+        "Argentina",
+        "Ternera",
+        "Raza Nostra",
+        "Deportista",
+        "de Pueblo",
+        "Barbacoa",
+        "Bacon",
+        "Madrid",
+        "Parmigiano",
+        "Ranchera",
+        "Ibérica",
+        "a la Antigua",
+        "Roquefort",
+        "Cebolla Pochada",
+        "Transilvania",
+        "Americana",
+        "del Chef",
+        "4 quesos",
+        "Setas"]
+    
+    func hamburguesaAleatoria() -> String{
+        let posicion = Int ( arc4random()) % hamburguesas.count
+        return hamburguesas[posicion]
+    }
+}
+
+class ColeccionPaises {
+    let paises = [
+        "México",
+        "Canadá",
+        "Estados Unidos",
+        "China",
+        "Brasil",
+        "Australia",
+        "India",
+        "Argentina",
+        "Kazajistán",
+        "Sudán",
+        "Argelia",
+        "República Democrática del Congo",
+        "Groenlandia",
+        "Arabia Saudita",
+        "Inglaterra",
+        "Indonesia",
+        "Libia",
+        "Irán",
+        "Mongolia",
+        "Perú",
+        ]
+    
+    func paisesAleatorios() -> String{
+        let posicion = Int ( arc4random()) % paises.count
+        return paises[posicion]
+    }
+    
+}
+
--- Hamburguesas en el mundo!/ViewController.swift
+++ Hamburguesas en el mundo!/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var hamburguesa: UILabel!
+    @IBOutlet weak var pais: UILabel!
+    
+    let hamburguesas = ColeccionHamburguesas()
+    let paises = ColeccionPaises()
+    let colores = Colores()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func dameHamburguesa(sender: AnyObject) {
+        let colorLoco = colores.coloresAleatorios()
+        let paisLoco = paises.paisesAleatorios()
+        let hamburguesaLoca = hamburguesas.hamburguesaAleatoria()
+        view.backgroundColor = colorLoco
+        view.tintColor = colorLoco
+        hamburguesa.text = hamburguesaLoca
+        pais.text = paisLoco
+    }
+    
 }
 
